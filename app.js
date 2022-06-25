ca8d920ab6e9a96ab4fa33da88dd59feea41bbd5
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-25 16:28:32 +0600

    server connected

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-test('should first', () => { second })
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get("/", (req, resp) => {
+    resp.send("Homepage")
+});
+
+app.listen(port, () => {
+    console.log(`app is running at http://localhost:${port}`);
+})
